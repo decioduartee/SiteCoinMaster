fa377f1d03a427569b6f2122617fe4d5e89af3ba
decioduartee <decioduarte.dev@gmail.com>
2021-11-14 01:56:12 -0300

    14|novembro

--- config/bot.js
+++ config/bot.js
@@ -2,7 +2,7 @@ require('../models/ListaDeDadosBot')
 const puppeteer = require('puppeteer'),
       mongoose  = require("mongoose"),
       DadosBot  = mongoose.model('botdados'),
-      dataLocal = new Date().toLocaleString(),
+      dataLocal = new Date().toLocaleString().substr(0, 10),
       cronJob = require('cron').CronJob;
 
 // Vai rodar todos os dias as 9h e as 14h
