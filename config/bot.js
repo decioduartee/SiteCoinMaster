68624d0dad8cab84b9a7ba04d1605332de52d93b
decioduartee <decioduarte.dev@gmail.com>
2021-11-16 01:30:16 -0300

    15|Novembro

--- config/bot.js
+++ config/bot.js
@@ -81,7 +81,7 @@ async function ligarBot() {
   list.forEach(async (items, i) => {
     await DadosBot.findOne({link: list[i].link}).lean().then((dados) => {
       if(dados) {
-        continue;
+        return
       } else {
         const novosDados = new DadosBot({
           titulo: list[i].titulo,
