8b14c5cfdefe11d75366b5862e1a63b8fdd309a1
decioduartee <decioduarte.dev@gmail.com>
2021-11-17 14:15:15 -0300

    17|Novembro

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 // Apaga mensagem de erros dentro de site
-var node = document.querySelector(".alert");
+const node = document.querySelector(".alert"),
+ano = new Date().getFullYear();
+// Mudando o ano anualmente no footer copyright
+let data = document.querySelector('.copyright')
+window.addEventListener('load', async () => {
+    data.innerHTML = `&copy; CliqueCoin | ${ano}`
+})
+
 setTimeout(() => {
     if (node.parentNode) {
         node.parentNode.removeChild(node);
